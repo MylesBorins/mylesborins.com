c08a5df0f16ec5d37acaf652ce0264cc412c9dff
Myles Borins <myles.borins@gmail.com>
2014-01-04 08:38:33 -0800

    Minor tweak

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,22 @@
 (function ($) {
   'use strict';
+  
+  var smally = $('.nav-links-bottom');
+  var children = smally.children();
+  smally.fadeTo(0, 0);
+  
   $('#header').stick_in_parent({
     parent: $('body')
   });
   
   $('#social-links').stick_in_parent({
 
+  }).on('sticky_kit:stick', function (e) {
+    smally.fadeTo('slow', 1);
+  }).on('sticky_kit:unstick', function (e) {
+    smally.fadeTo('slow', 0.0);
   });
-  
-  var smally = $('.nav-links-bottom');
-  
-  var children = smally.children();
-  
+    
   children.each(function (i) {
     var icon = $(children[i]);
     icon.fadeTo(0, 0.2);
