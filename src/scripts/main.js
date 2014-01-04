3df7e84ffafb5eefcf386718a997f9fbe9914e17
Myles Borins <myles.borins@gmail.com>
2014-01-04 08:28:50 -0800

    Looks not as gross

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,11 +4,12 @@
     parent: $('body')
   });
   
-  $('#nav-wrapper').stick_in_parent({
+  $('#social-links').stick_in_parent({
 
   });
   
-  var smally = $('.nav-links-small');
+  var smally = $('.nav-links-bottom');
+  
   var children = smally.children();
   
   children.each(function (i) {
@@ -19,29 +20,5 @@
     }, function (e) {
       icon.fadeTo('slow', 0.2);
     });
-    console.log(icon);
   });
-  
-  /*for (var i in children) {
-    if (children.hasOwnProperty(i)) {
-      var icon = $(children[i]);
-      icon.hover(function (e) {
-        icon.fadeTo('fast', 1.0);
-      }, function (e) {
-        icon.fadeTo('slow', 0.2);
-      });
-    }
-  }*/
-  /*forEach(function (icon) {
-    icon.hover(function (e) {
-      icon.fadeTo('fast', 1.0);
-    }, function (e) {
-      icon.fadeTo('slow', 0.2);
-    });
-  });*/
-  /*smally.hover(function (e) {
-    smally.fadeTo('fast', 1.0);
-  }, function (e) {
-    smally.fadeTo('slow', 0.2);
-  });*/
 }(jQuery));
